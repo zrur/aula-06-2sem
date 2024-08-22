99fd0da3a46672f0e542d69a097e970c6294e556
TamaraBoni <tamara_boni@hotmail.com>
2024-08-22 20:12:38 -0300

    Construindo o objeto de rotas em mais.tsx

---
+++ aula06/src/main.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+
+
+import { RouterProvider, createBrowserRouter}from 'react-router-dom';
+import Home from './routes/Home/index.tsx';
+import Produtos from './routes/Produtos/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+import Erro from './routes/Erro/index.tsx';
+
+const routes = createBrowserRouter([
+  {
+    path:"/", element:<App/>, errorElement: <Erro/>, children:[
+      {path:"/", element:<Home/>},
+      {path:"/produtos", element:<Produtos/>},
+      {path:"/edita/produtos/:id", element:<EditarProdutos/>},
+   ]
+  }
+]);
+
+createRoot(document.getElementById ('root')!).render(
+  <StrictMode>
+    <RouterProvider router={routes} />
+  </StrictMode>
+)
---
+++ aula06/src/routes/Erro/index.tsx
@@ -0,0 +1,7 @@
+export default function Erro() {
+    return (
+        <div>
+            <h1> Componente Erro</h1>
+        </div>
+    );
+}
