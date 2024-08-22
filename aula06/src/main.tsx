1125a92aaded04b7d260dd6d9ff63bd8d95a9af5
HeitorOrtega <rm557825@fiap.com.br>
2024-08-22 20:10:38 -0300

    Construindo as rotas

--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -2,14 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from "./routes/Home/index.tsx";
 import Produtos from "./routes/Produtos/index.tsx";
 import EditarProdutos from "./routes/EditarProdutos/index.tsx";
 import Error from "./routes/Error/index.tsx";
 
+const routes = createBrowserRouter([
+  {path: "/" , element:<App/>, errorElement:<Error />, children:[
+    {path:"/" , element: <Home/>},
+    {path: "produtos",element:<Produtos/>},
+    {path:"/editar/produtos/:id", element:<EditarProdutos/>}
+    ]
+  }
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
